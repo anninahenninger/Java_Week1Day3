8e0fa87290ac08a7ac7842675c88751bb693c4bf
Judith Lang-Ladurner <judithmarialang@gmail.com>
2019-05-08 10:33:04 +0200

    adds BasicA2

---
+++ src/Basic_A2.java
@@ -0,0 +1,10 @@
+//A2: How long is the string returned by the following expression? What is the string?
+//
+//"Was it a car or a cat I saw?".substring(9, 12)
+
+public class Basic_A2 {
+    public static void main(String[] args) {
+        String small = "Was it a car or a cat I saw.".substring(9,12);
+        System.out.println(small);
+    }
+}
