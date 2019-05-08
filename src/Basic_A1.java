330448c3d9ce267823a209abdcdf1007f99d45bf
Annina Henninger <annina.henninger@aon.at>
2019-05-08 09:39:38 +0200

    Initial commit

---
+++ src/Basic_A1.java
@@ -0,0 +1,17 @@
+/**
+ * Consider the following string:
+ *
+ * String hannah = "Did Hannah see bees? Hannah did.";
+ *
+ *     What is the value displayed by the expression hannah.length()?
+ *
+ *     What is the value returned by the method call hannah.charAt(12)?
+ *
+ *     Write an expression that refers to the letter b in the string referred to by hannah.
+ */
+public class Basic_A1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
